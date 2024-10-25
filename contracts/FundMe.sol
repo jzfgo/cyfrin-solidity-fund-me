a612051eb04096f0bf84881699b1e52ac1a5d57a
jzfgo <javierzapata82@gmail.com>
2024-10-25 16:58:24 +0200

    Section 3: Fund Me - Lesson 30: Congratulations

--- contracts/FundMe.sol
+++ contracts/FundMe.sol
@@ -1,22 +1,55 @@
 // SPDX-License-Identifier: MIT
-
 pragma solidity ^0.8.24;
 
+import { AggregatorV3Interface } from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
 import { PriceConverter } from "./PriceConverter.sol";
+
+error FundMe__NotOwner();
 
 contract FundMe {
     using PriceConverter for uint256;
 
-    uint256 public minimumUsd = 5e18;
+    uint256 public constant MINIMUM_USD = 5e18;
+    address private immutable i_owner;
 
-    address[] public funders;
-    mapping(address funder => uint256 amountFunded) public addressToAmountFunded;
+    address[] private s_funders;
+    mapping(address => uint256) public s_addressToAmountFunded;
+    AggregatorV3Interface private s_priceFeed;
 
-    function fund() public payable {
-        require(msg.value.getConversionRate() >= minimumUsd, "didn't send enough ETH");
-        funders.push(msg.sender);
-        addressToAmountFunded[msg.sender] = addressToAmountFunded[msg.sender] + msg.value;
+    modifier onlyOwner() {
+        // require(msg.sender == i_owner);
+        if (msg.sender != i_owner) revert FundMe__NotOwner();
+        _; // execute the code within the function
     }
 
-    // function withdraw() public {}
+    constructor() {
+        s_priceFeed = AggregatorV3Interface(s_priceFeed);
+        i_owner = msg.sender;
+    }
+
+    function fund() public payable {
+        require(msg.value.getConversionRate() >= MINIMUM_USD, "didn't send enough ETH");
+        s_addressToAmountFunded[msg.sender] += msg.value;
+        s_funders.push(msg.sender);
+    }
+
+    function withdraw() public onlyOwner {
+        for(uint256 i = 0; i < s_funders.length; i++) {
+            address funder = s_funders[i];
+            s_addressToAmountFunded[funder] = 0;
+        }
+
+        s_funders = new address[](0);
+
+        (bool callSuccess,) = payable(msg.sender).call{value: address(this).balance}("");
+        require(callSuccess, "Call failed");
+    }
+
+    receive() external payable {
+        fund();
+    }
+
+    fallback() external payable {
+        fund();
+    }
 }
